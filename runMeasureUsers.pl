3746db28f9b3995c02fe565c5792841cac9484d7
cognus <cognus@gmail.com>
2010-06-16 16:47:40 -0500

    Add Perl script and input for monitoring Memory usage on a Linux System

---
+++ runMeasureUsers.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+use warnings;
+
+open(USERS, "/usr/local/scripts/perl/users") or die "Can not open the file \"users\"!!! $!";
+
+while(my $user = <USERS>){
+    chomp(my $USER = $user);
+
+    @procesos = `ps --no-headers -u $USER -o rss,pid,etime,command --sort rss`;
+    
+    print '-' x 128, "\n";
+    print "Data for $USER\n";
+    print "Memory(MB)\tPID\tCOMMAND\n";
+          
+    print '-' x 128, "\n";
+    
+    foreach my $proc(@procesos){
+        my @procesos = (my $stuff, my $mmem, my $pid, my $etimeCommand) = split /(\d+)\s+(\d+)\s+(.+)/, $proc;
+        $mem = $mmem / 1024;
+        print $mem, "\t",$pid, "\t",$etimeCommand,"\n";
+        $memoria{$USER}.= $mem . " ";
+    }
+    
+    foreach $USER(sort keys %memoria){
+        $procesos = 0;
+        $total = 0;
+        my @valores = split(" ", $memoria{$USER});
+        
+        foreach my $proceso(@valores){
+            $procesos++;
+            $total += $proceso;
+        }
+    }
+    
+    print "Total memory usage for user ", $USER, ":\t", $total, " MB\n";
+}
+
+print '-' x 128, "\n";
